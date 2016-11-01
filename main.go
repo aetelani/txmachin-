ae3a24cce30bc9f6179d67c97aee8034fd05280c
Anssi Eteläniemi <aetelani@live.com>
2016-11-02 00:34:47 +0200

    changed uuid to struct

--- main.go
+++ main.go
@@ -4,38 +4,60 @@ package main
 #cgo CFLAGS: -I /usr/include/
 #include <uuid/uuid.h>
 #include <stdlib.h>
+#include <errno.h>
 int p_sizeOfUuid = sizeof(uuid_t);
 int p_bufLen = -1;
 int p_nextTokenIndex = 0;
 unsigned char * p_UuidBuf;
-unsigned char * allocUuidBuf(const int bufLen) {
+
+void allocUuidBuf(const int bufLen) {
 	p_bufLen = bufLen;
 	p_UuidBuf = malloc(p_sizeOfUuid * bufLen);
-	return p_UuidBuf;
 }
+
 void freeUuidBuf() {
 	free(p_UuidBuf);
 }
+
 void populateBuf() {
 	for(int i=0; i < p_bufLen; i++) {
 		uuid_generate_random(p_UuidBuf + i * p_sizeOfUuid);
 	}
 }
+
 unsigned char * getNextToken() {
+	if(p_nextTokenIndex == p_bufLen) {
+		errno = EIO;
+		return 0;
+	}
 	return (p_UuidBuf + p_nextTokenIndex++ * p_sizeOfUuid);
 }
 */
 import "C"
 import "unsafe"
 import "fmt"
+//import "bytes"
+
+type uuid struct { }
+
+func (u uuid) New() ([]byte) {
+	var _, err = C.allocUuidBuf(1)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return nil
+	}
+	defer C.freeUuidBuf()
+	_, err = C.populateBuf()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return nil
+	}
+	b := C.GoBytes(unsafe.Pointer(C.getNextToken()), C.p_sizeOfUuid)	
+	return b
+}
 
 func main() {
-	C.allocUuidBuf(10)
-	C.populateBuf()	
-	fmt.Println("hello uuid:", C.GoBytes(unsafe.Pointer(C.getNextToken()), C.p_sizeOfUuid))
-	fmt.Println("hello uuid:", C.GoBytes(unsafe.Pointer(C.getNextToken()), C.p_sizeOfUuid))
-	fmt.Println("hello uuid:", C.GoBytes(unsafe.Pointer(C.getNextToken()), C.p_sizeOfUuid))
-	fmt.Println("hello uuid:", C.GoBytes(unsafe.Pointer(C.getNextToken()), C.p_sizeOfUuid))
-	defer C.freeUuidBuf()
+	s := uuid{}
+	fmt.Println("hello uuid:", s.New())	
 	return
 }
