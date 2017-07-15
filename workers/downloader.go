d1aeddc53411e667672c7b4cc6e7951ef2ecf2c0
Anssi Eteläniemi <aetelani@live.com>
2017-07-15 16:00:44 +0300

    Moved request to work closure and retuned context

--- workers/downloader.go
+++ workers/downloader.go
@@ -9,18 +9,30 @@ import (
 type foo struct { url string }
 
 func Downloader(ctx context.Context, desc interface{}) (func() (interface{}, error)) {
-	log.Println("downloader")
+	log.Println("Initializing Downloader")
 	url := desc.(string)
-	response, err := http.Get(url);
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer response.Body.Close()
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
 	
-	return func() (interface{}, error) { return responseData, nil }
+	// use togen and set cancel
+	localCtx := context.WithValue(ctx, "id",byte('A'))
+
+	return func() (interface{}, error) { 
+
+		response, err := http.Get(url);
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer response.Body.Close()
+
+		responseData, err := ioutil.ReadAll(response.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		localCtx = context.WithValue(localCtx, "responseData", responseData)
+
+		return localCtx, nil
+		}
 }
